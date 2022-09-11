3f469c55a97cd0df09803e22ae6fcb327f95e364
Amiculay <aaronnguyen1123@gmail.com>
2022-09-11 09:42:34 -0400

    Removed init

--- flask_app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-app = Flask(__name__)
-app.secret_key = "ohsnapthisisprettycrazyredifyiswild"
-
-DATABASE = 'redify'
-
-SPOTIFY_APP_ID = os.getenv("APP_ID")
-SPOTIFY_APP_SECRET = os.getenv("APP_SECRET")
